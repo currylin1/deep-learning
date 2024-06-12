5f62488e1f70919c283f47f5f0d95ae36f7a40a3
Python <141690879+currylin1@users.noreply.github.com>
2024-06-12 16:23:55 +0800

    放置第二位子圖檔
    project_window 用來將選取的圖放置視窗 (path_2)

---
+++ PictureLoad_2.py
@@ -0,0 +1,48 @@
+import platform
+
+import cv2
+import numpy as np
+from PIL import ImageFont, ImageDraw,Image
+from PySide6.QtCore import QThread, Signal
+from PySide6.QtGui import QPixmap
+
+
+class PictureLoad_2(QThread):
+    callback=Signal(object)
+
+    def __init__(self,path_2,parent=None):
+        super().__init__(parent)
+        self.path_2=path_2
+        self.runFlag = True
+
+
+    def text(self, img, text, xy=(0, 0), color=(0, 0, 0), size=12):
+        pil = Image.fromarray(img)
+        s = platform.system()
+        if s == "Linux":
+            font = ImageFont.truetype(
+                '/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc',
+                size)
+        elif s == "Darwin":
+            font = ImageFont.truetype('....', size)
+        else:
+            font = ImageFont.truetype('simsun.ttc', size)
+        ImageDraw.Draw(pil).text(xy, text, font=font, fill=color)
+        return np.array(pil)
+
+
+
+    def run(self): #新執行續要做的工作
+        img = cv2.imdecode(
+            np.fromfile(self.path_2, dtype=np.uint8),
+            cv2.IMREAD_COLOR
+        )[:, :, ::-1].copy()
+        self.callback.emit(img)
+
+
+
+        index=0
+
+
+
+
